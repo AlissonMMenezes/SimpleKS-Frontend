26b43e786855c6b4fc7e0ba3349e255113f7baf5
Alisson Menezes <alisson.copyleft@gmail.com>
2020-11-30 08:12:51 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,8 @@ margin: 20px 0 5px 0;
 margin-top: 20px;
 padding: 10px 0;
 padding-top: 10px;
+}
+body {
 white-space: pre-wrap !important; 
 word-wrap: break-word !important;
 font-family: inherit !important;
