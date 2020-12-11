730c34251c856e013f11a08f4f011423446c7cfc
Alisson Machado <alisson.copyleft@gmail.com>
2020-12-11 20:34:58 +0100

    fixing header links

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -7,7 +7,7 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav  v-for="p in info.pages" :key="p">
-                <a class="nav-link" v-bind:href="p.post_name" tag="b-nav-item">{{p.title}}</a>
+                <a class="nav-link" v-bind:href="'/'+p.post_name" tag="b-nav-item">{{p.title}}</a>
             </b-navbar-nav>
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
