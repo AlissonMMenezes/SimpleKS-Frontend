979b49f9afc7a5631d49a7d655eed2b7ea2498f8
Alisson Machado <alisson.copyleft@gmail.com>
2020-12-23 21:11:27 +0100

    cards fixed and thumbnails fixed

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,9 +1,9 @@
 <template>
     <div style="margin-left: 10px;" >
-        <div v-for="p in info.posts" :key="p" style="margin-left: 10px; float: left; display: block;    " class="overflow-hidden">
-        <b-card  style="margin-top: 10px; max-height: 200px; max-width: 650px;">
-            <b-row no-gutters>            
-            <b-col md="8">
+        <div v-for="p in info.posts" :key="p" style="margin-left: 10px; float: left; display: block;" class="overflow-hidden">
+        <b-card  style="margin-top: 10px; max-height: 200px; width: 650px;">
+            <b-row>            
+            <b-col md="7">
                 <b-card-body>
                     <b-card-title>
                         <a v-bind:href="'/'+p.post_name" >{{p.title}}</a>
@@ -14,9 +14,9 @@
 
                 </b-card-body>
             </b-col>
-            <b-col md="4">
-                <b-card-img v-if="p.thumbnail != null" :src="p.thumbnail" alt="Image" class="rounded-0" style="wdith: 209.333px; height: 175.283px"></b-card-img>
-                <b-card-img v-else :src="p.category.thumbnail" alt="Image" class="rounded-0"></b-card-img>
+            <b-col md="5">
+                <b-card-img v-if="p.thumbnail" :src="p.thumbnail" alt="Image" class="rounded-0" style="width: 210px; height: 175px"></b-card-img>
+                <b-card-img v-else :src="p.category.thumbnail" alt="Image" class="rounded-0" style="width: 210px; height: 175px"></b-card-img>
 
             </b-col>
             </b-row>
