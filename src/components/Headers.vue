d168a02886bd4ec9b94b4d039fdac2a17df606dd
Alisson Menezes <alisson.copyleft@gmail.com>
2021-05-17 10:40:30 +0200

    Update Headers.vue

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -7,7 +7,7 @@
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav  v-for="p in info.pages" :key="p">
-                    <b-nav-item class="nav-link" v-bind:href="'/'+p.page_name">{{p.title}}</b-nav-item>
+                    <b-nav-item class="nav-link" v-bind:href="'/'+p.page_name/">{{p.title}}</b-nav-item>
                 </b-navbar-nav>
                 <b-navbar-nav class="ml-auto">
                     <b-form-input size="sm" v-on:keyup.enter="search"  class="mr-sm-2" v-model="term" :placeholder="term"></b-form-input>
